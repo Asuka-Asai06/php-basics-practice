aaa782b72aad146c6f41d1352fdfac966527ea55
Asuka Asai <asaiasuka06@gmail.com>
2026-06-03 15:29:50 +0900

    Initial commit

---
+++ src/7-1-6_hands-on/practice/price_calculator.php
@@ -0,0 +1,20 @@
+<?php
+$product_name = "ノートパソコン";
+$price = 80000;
+$quantity = 2;
+$tax_rate = 0.1;
+
+$subtotal = $price * $quantity;
+$tax_amount = $subtotal * $tax_rate;
+$total = $subtotal + $tax_amount;
+
+$subtotal = number_format($subtotal);
+$tax_amount = number_format($tax_amount);
+$total = number_format($total);
+
+echo "商品名: {$product_name}<br>";
+echo "単価: " . number_format($price) . "円<br>";
+echo "数量: {$quantity}個<br>";
+echo "小計: {$subtotal}円<br>";
+echo "消費税(10%): {$tax_amount}円<br>";
+echo "<strong>合計金額: {$total}円</strong><br>";
